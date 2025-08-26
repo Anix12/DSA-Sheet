7455cfbe45b8357dd3a029f349de79f7f42e24ed
Anix12 <adhavaniket1221@gmail.com>
2025-08-26 10:14:01 +0530

    Array-StockPrice

---
+++ src/Arrays/StockBuySell.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class StockBuySell {
+    //Brute Force Approch
+    public static int stockPrice(int arr[]){
+        int maxValu=0;
+        int sum;
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+               sum=arr[j]-arr[i];
+               maxValu=Math.max(maxValu, sum);
+            }
+        }
+        return maxValu;
+    }
+
+    //Optimiza Approch
+
+
+    public static void main(String[] args) {
+        int arr[]={7,1,5,3,6,4};
+        System.out.println(stockPrice(arr));
+    }
+}
