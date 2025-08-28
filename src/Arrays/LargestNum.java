d87728c51abc51743d3a9184f5eb54efbbf89404
Anix12 <adhavaniket1221@gmail.com>
2025-08-28 18:47:52 +0530

    Largest Number Update

--- src/Arrays/LargestNum.java
+++ src/Arrays/LargestNum.java
@@ -1,15 +1,28 @@
 package Arrays;
 
 import java.util.Arrays;
+
 public class LargestNum {
 
-    public static int LargestNumber(int arr[]){
+    //Brute Force
+    public static int LargestNum(int arr[]) {
+        int maxNum = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > maxNum) {
+                maxNum = arr[i];
+            }
+        }
+        return maxNum;
+    }
+
+    //Optimize Solution
+    public static int LargestNumber(int arr[]) {
         Arrays.sort(arr);
-        return arr[arr.length-1];
+        return arr[arr.length - 1];
     }
 
     public static void main(String[] args) {
-       int arr[]={5,6,2,3,1,4};
+        int arr[] = {-1, -2, -3, -4, -3};
         System.out.println(LargestNumber(arr));
     }
 }
