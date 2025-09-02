7aeb3c0133147bd8afeb2b38adf63b8896954294
Anix12 <adhavaniket1221@gmail.com>
2025-09-02 20:14:40 +0530

    Rotate Matrix Optimal Solution

--- src/Arrays/RotateMatrix.java
+++ src/Arrays/RotateMatrix.java
@@ -3,30 +3,46 @@ package Arrays;
 import java.util.ArrayList;
 
 public class RotateMatrix {
-
+    //Brute Force Approch
     public static void rotateMatrix(int matrix[][]) {
-        ArrayList<ArrayList<Integer>> list=new ArrayList<>();
-        for(int colm=0; colm<matrix[0].length; colm++){
-            ArrayList<Integer> list1=new ArrayList<>();
-            for(int row=matrix.length-1; row>=0; row--){
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        for (int colm = 0; colm < matrix[0].length; colm++) {
+            ArrayList<Integer> list1 = new ArrayList<>();
+            for (int row = matrix.length - 1; row >= 0; row--) {
                 list1.add(matrix[row][colm]);
             }
             list.add(list1);
         }
-
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix[0].length; j++){
-                matrix[i][j]=list.get(i).get(j);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[i][j] = list.get(i).get(j);
             }
         }
+    }
 
+    //Optimal Approch
+    public static void rotateMat(int arr[][]) {
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j < arr[0].length; j++) {
+                if (i != j) {
+                    swap(arr, i, j);
+                }
+            }
+        }
+    }
+
+    public static void swap(int arr[][], int i, int j){
+        //swapping Function
+        int temp=arr[i][j];
+        arr[i][j]=arr[j][i];
+        arr[j][i]=temp;
     }
 
     //print Array
-    public static void printArray(int matrix[][]){
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix[0].length; j++){
-                System.out.print(matrix[i][j]+" ");
+    public static void printArray(int matrix[][]) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
@@ -39,7 +55,7 @@ public class RotateMatrix {
                 {4, 5, 6},
                 {7, 8, 9}
         };
-       rotateMatrix(arr);
-       printArray(arr);
+        rotateMat(arr);
+        printArray(arr);
     }
 }
