423c820ee2d736ef3d2aa295941b0e9bd01016fd
Anix12 <adhavaniket1221@gmail.com>
2025-09-04 11:00:39 +0530

    Merge Array Brute And Optimal Solution Added

--- src/Arrays/MergeArray.java
+++ src/Arrays/MergeArray.java
@@ -1,28 +1,72 @@
 package Arrays;
-
+import java.util.Arrays;
 public class MergeArray {
     //Merge two Sorted Arrays Without Extra Space
+    public static int[] mergeArrWithExtraSpace(int arr1[], int arr2[]) {
+        int m = arr1.length;
+        int n = arr2.length;
+        int arr[] = new int[m + n];
 
-    public static void main(String[] args) {
-        int[] arr1 = {1, 4, 8, 10};
-        int[] arr2 = {2, 3, 9};
-        int len = arr1.length + arr2.length;
-        int[] arr3 = new int[len];
-
-        int lp = 0, rp = 0, i = 0;
-        while (lp < arr1.length && rp < arr2.length) {
+        int lp = 0, rp = 0;
+        int idx = 0;
+        while (lp < m && rp < n) {
             if (arr1[lp] < arr2[rp]) {
-                arr3[i] = arr1[lp];
+                arr[idx] = arr1[lp];
                 lp++;
             } else {
-                arr3[i] = arr2[rp];
+                arr[idx] = arr2[rp];
                 rp++;
             }
-            i++;
+            idx++;
         }
-        for (int k = 0; k < arr3.length; k++) {
-            System.out.print(arr3[k] + " ");
+        if (lp != arr1.length) {
+            while (lp < m) {
+                arr[idx] = arr1[lp];
+                lp++;
+                idx++;
+            }
+        } else if (rp != arr2.length) {
+            while (rp < n) {
+                arr[idx] = arr2[rp];
+                rp++;
+                idx++;
+            }
         }
+        return arr;
+    }
+
+    //Merge Sort Without Extra Space Tc-O(n)
+    public static void mergeSort(int arr1[], int arr2[]) {
+        int lp = arr1.length - 1, rp = 0;
+        while (arr1[lp] > arr2[rp]) {
+            swap(arr1, arr2, lp, rp);
+            lp--;
+            rp++;
+        }
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+    }
+
+    public static void swap(int arr1[], int arr2[], int lp, int rp) {
+        //swap
+        int temp = arr1[lp];
+        arr1[lp] = arr2[rp];
+        arr2[rp] = temp;
+    }
+
+    public static void printArray(int matrix[]) {
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.print(matrix[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr1[] = {1 ,4 ,8 ,10};
+        int arr2[] = {2 ,3 ,9};
+        mergeSort(arr1, arr2);
+        printArray(arr1);
+        System.out.println();
+        printArray(arr2);
     }
 
 }
