5fb101cfcab4045e29363330b3d928bdf2f19096
Anix12 <adhavaniket1221@gmail.com>
2025-09-05 12:23:27 +0530

    Majority Element N/3 Element Brute Force and Better Solution Updated

--- src/ArrayPart3/MajorityNbythree.java
+++ src/ArrayPart3/MajorityNbythree.java
@@ -1,16 +1,35 @@
 package ArrayPart3;
 
-import java.util.ArrayList;
+import java.util.*;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MajorityNbythree {
 
     //Brute Force Approch
+    public static List<Integer> MajorElement(int arr[]) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (list.isEmpty() || list.get(0) != arr[i]) {
+                int count = 0;
+                for (int j = 0; j < arr.length; j++) {
+                    if (arr[i] == arr[j]) {
+                        count++;
+                    }
+                }
+                if(count>arr.length/3){
+                    list.add(arr[i]);
+                }
+            }
+        }
+        return list;
+    }
 
+
+    //Better Approch
     public static ArrayList<Integer> MajorityElement(int arr[]) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        ArrayList<Integer> list =new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
         for (int key : arr) {
             map.put(key, map.getOrDefault(key, 0) + 1);
         }
@@ -24,7 +43,7 @@ public class MajorityNbythree {
 
 
     public static void main(String[] args) {
-        int arr[] = {2,2};
-        System.out.println(MajorityElement(arr));
+        int arr[] = {3, 3, 4, 2, 4, 4, 2, 4, 4};
+        System.out.println(MajorElement(arr));
     }
 }
