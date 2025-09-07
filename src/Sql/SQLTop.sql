29bc085fbcfa6a36e7d65ad74e5214fa3315f2a5
Anix12 <adhavaniket1221@gmail.com>
2025-09-07 19:40:20 +0530

    New Module is Added in Scrach for Sql Practice

---
+++ src/Sql/SQLTop.sql
@@ -0,0 +1,9 @@
+//Its a Practice Set For Top 50 Sql Queries which as in cognizant Technical Assesment
+
+// 1.Print All Records in Sql EmployeeDetails Table
+    SELECT * FROM Table_Name;
+
+//2.Print All Records Who Employee ID 1
+    SELECT * FROM Table_Name
+    WHERE EMP_ID =1;
+//3.
