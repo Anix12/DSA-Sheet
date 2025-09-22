4e42984f03a2d9a67ce19050f34e979660664103
Anix12 <adhavaniket1221@gmail.com>
2025-09-22 11:14:51 +0530

    This And Super Keyword Module Added in OOPS

---
+++ src/OOPS/ThisAndSuper.java
@@ -0,0 +1,21 @@
+package OOPS;
+
+class Person {
+    void Walk() {
+        System.out.println("Person Walking ");
+    }
+}
+
+class Man extends Person {
+    void Walk() {
+        super.Walk();
+    }
+}
+
+public class ThisAndSuper {
+
+    public static void main(String[] args) {
+        Man m=new Man();
+        m.Walk();
+    }
+}
