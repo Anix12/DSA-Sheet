fa283ba689854c3f8eb016df16dc27cecf81df1f
Anix12 <adhavaniket1221@gmail.com>
2025-09-25 18:40:38 +0530

    Two Sum optimal  Solution Added in ArrayPart4 Module

--- src/ArrayPart4/TwoSum.java
+++ src/ArrayPart4/TwoSum.java
@@ -19,15 +19,31 @@ public class TwoSum {
         return new int[]{-1, -1};
     }
 
+    //Optimal Approch - if we want pair values
+    public static int[] OptimalSolTwoSum(int arr[], int target) {
+        Arrays.sort(arr);
+        int lp = 0, rp = arr.length - 1;
+        while (lp < rp) {
+            if (arr[lp] + arr[rp] == target) {
+                return new int[]{arr[lp], arr[rp]};
+            } else if (arr[lp] + arr[rp] < target) {
+                lp++;
+            } else {
+                rp--;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
     public static void main(String[] args) {
         int arr[] = {2, 6, 5, 8, 11};
         int target = 14;
-        int result[] = twosum(arr, target);
+        int result[] = OptimalSolTwoSum(arr, target);
         if (result[0] != -1) {
             System.out.println(true);
             System.out.println(Arrays.toString(result));
         } else {
-            System.out.println(true);
+            System.out.println(false);
             System.out.println(Arrays.toString(result));
         }
     }
