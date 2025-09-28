94ef2fcdb31bac1a0fc88f88acddd56a76d8fce3
Anix12 <adhavaniket1221@gmail.com>
2025-09-28 10:24:50 +0530

    4 Sum Solution Added in ArrayPart4 Module

--- src/ArrayPart4/F4Sum.java
+++ src/ArrayPart4/F4Sum.java
@@ -5,24 +5,35 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class F4Sum {
-    //Brute Force
-    public static Set findTarget(int arr[], int target) {
-        Set<ArrayList<Integer>> list = new HashSet<>();
-        for (int i = 0; i < arr.length-3; i++) {
-            for (int j = i + 1; j < arr.length-2; j++) {
-                for (int k = j + 1; k < arr.length-1; k++) {
-                    for (int l = k + 1; l < arr.length; l++) {
-                        ArrayList<Integer> list1 = new ArrayList<>();
-                        int total = arr[i] + arr[j] + arr[k] + arr[l];
-                        if (total == target) {
-                            list1.add(arr[i]);
-                            list1.add(arr[j]);
-                            list1.add(arr[k]);
-                            list1.add(arr[l]);
-                            list.add(list1);
-                        }
+
+    public static ArrayList<ArrayList<Integer>> findTarget(int arr[], int target) {
+        Arrays.sort(arr);
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (i > 0 && arr[i] == arr[i - 1]) continue;
+            for (int j = i + 1; j < arr.length; ) {
+                int p = j + 1, q = arr.length - 1;
+                while (p < q) {
+                    ArrayList<Integer> sublist = new ArrayList<>();
+                    long sum = arr[i] + arr[j];
+                    sum += arr[p] + arr[q];
+                    if (sum < target) {
+                        p++;
+                    } else if (sum > target) {
+                        q--;
+                    } else {
+                        sublist.add(arr[i]);
+                        sublist.add(arr[j]);
+                        sublist.add(arr[p]);
+                        sublist.add(arr[q]);
+                        list.add(sublist);
+                        p++;
+                        q--;
+                        while (p < q && arr[p] == arr[p - 1]) p++;
                     }
                 }
+                j++;
+                while (j < arr.length && arr[j] == arr[j - 1]) j++;
             }
         }
         return list;
@@ -30,8 +41,7 @@ public class F4Sum {
 
 
     public static void main(String[] args) {
-        int arr[] = {4, 3, 3, 4, 4, 2, 1, 2, 1, 1};
-
-        System.out.println(findTarget(arr, 9));
+        int arr[] = {-2, -1, -1, 1, 1, 2, 2};
+        System.out.println(findTarget(arr, 0));
     }
 }
