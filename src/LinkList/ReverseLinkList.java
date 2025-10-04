ae274962a022d014b2dd55e7812a0b972fa9d6d6
Anix12 <adhavaniket1221@gmail.com>
2025-10-04 19:28:13 +0530

    New LinkList Topic Starte

---
+++ src/LinkList/ReverseLinkList.java
@@ -0,0 +1,48 @@
+package LinkList;
+
+import java.util.LinkedList;
+
+public class ReverseLinkList {
+
+    // Define the Node class for the singly linked list
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+
+    public static Node reverseLinkedList(Node head) {
+        Node prev = null;
+        Node curr = head;
+        Node next;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+
+        Node curr = reverseLinkedList(head);
+
+        while (curr != null) {
+            System.out.print(curr.data + "->");
+            curr=curr.next;
+        }
+        System.out.println("null");
+    }
+}
