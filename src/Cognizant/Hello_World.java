fe13e5f8b8b49c293576771780ed22db1eb19e2a
Anix12 <adhavaniket1221@gmail.com>
2025-10-07 19:39:16 +0530

    Solution added in Cognizant Module

--- src/Cognizant/Hello_World.java
+++ src/Cognizant/Hello_World.java
@@ -4,8 +4,6 @@ public class Hello_World {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Hello World");
-        }
+        System.out.println("Hello Aniket");
     }
 }
