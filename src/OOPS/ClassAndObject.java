28a64f734ff5a1ac5bacd9ee0ef4cdd6449b25bc
Anix12 <adhavaniket1221@gmail.com>
2025-10-09 18:38:11 +0530

    Classes And Object Module Added in OOPS Module

---
+++ src/OOPS/ClassAndObject.java
@@ -0,0 +1,14 @@
+package OOPS;
+
+class Student {
+    String name;
+    int roll_no;
+
+    String get_Name(String name) {
+        return this.name = name;
+    }
+}
+
+public class ClassAndObject {
+
+}
