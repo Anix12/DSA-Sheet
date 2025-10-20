3d4769b7cd015252169476cac492072a6686ac78
Anix12 <adhavaniket1221@gmail.com>
2025-10-20 10:41:27 +0530

    Remove Duplicate Better Solution added in Array Part 5 Module

--- src/ArrayPart5/RemoveDuplicate.java
+++ src/ArrayPart5/RemoveDuplicate.java
@@ -3,7 +3,19 @@ package ArrayPart5;
 import java.util.*;
 
 public class RemoveDuplicate {
+    //better Solution Tc-O(n)
+    public static int duplicateRemove(int arr[]) {
+        int count = 1;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
+                count++;
+            }
+        }
+        return count;
+    }
 
+
+    // TC- O(n) and SC-O(n)
     public static int removeDuplicate(int arr[]) {
         HashSet<Integer> set = new HashSet<>();
         for (int i = 0; i < arr.length; i++) {
@@ -13,7 +25,7 @@ public class RemoveDuplicate {
     }
 
     public static void main(String[] args) {
-        int arr[] = {1, 1, 1, 2, 2, 3, 3, 3, 3, 4, 4, 5};
-        System.out.println(removeDuplicate(arr));
+        int arr[] = {1, 2, 3, 4, 5};
+        System.out.println(duplicateRemove(arr));
     }
 }
