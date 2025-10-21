a0c4966c47423c833e2237da47e2c55d51a88422
Anix12 <adhavaniket1221@gmail.com>
2025-10-21 17:42:48 +0530

    Remove Duplicate Solution

--- src/ArrayPart5/RemoveDuplicate.java
+++ src/ArrayPart5/RemoveDuplicate.java
@@ -3,16 +3,7 @@ package ArrayPart5;
 import java.util.*;
 
 public class RemoveDuplicate {
-    //better Solution Tc-O(n)
-    public static int duplicateRemove(int arr[]) {
-        int count = 1;
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] != arr[i - 1]) {
-                count++;
-            }
-        }
-        return count;
-    }
+
 
 
     // TC- O(n) and SC-O(n)
@@ -26,6 +17,6 @@ public class RemoveDuplicate {
 
     public static void main(String[] args) {
         int arr[] = {1, 2, 3, 4, 5};
-        System.out.println(duplicateRemove(arr));
+        System.out.println(removeDuplicate(arr));
     }
 }
